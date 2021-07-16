679c9fdba5c9309bbd73363a8bd80d54d8d7fbee
wijeremy <jeremydavwilliams@gmail.com>
2021-07-15 20:22:06 -0500

    okay for real this time

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
 var PORT = process.env.PORT || 3001;
 
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "index.html"));
-  });
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(PORT, function() {return})
+app.listen(PORT, function() {console.log("app listening")})
